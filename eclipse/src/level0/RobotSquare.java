03a1f831415c6f439e87501f8c8ac4345ba8496c
raeesbb8 <league@Mac-mini-2.local>
2016-09-08 18:31:07 -0700

    i made a square

--- eclipse/src/level0/RobotSquare.java
+++ eclipse/src/level0/RobotSquare.java
@@ -6,16 +6,16 @@ public class RobotSquare {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
 		rob.penDown();
-		rob.setSpeed(350);
+		rob.setSpeed(300);
 		rob.setRandomPenColor();
 		for (int i = 0; i < 80; i = i + 1) {
 			
-			rob.move(200);
-			rob.turn(600);
+			rob.move(300);
+			rob.turn(90);
 
 			for (int j = 0; j < 4; j = j + 1) {
-				rob.move(200);
-				rob.turn(900);
+				rob.move(300);
+				rob.turn(90);
 			}
 		}
 	}
